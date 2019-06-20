1111242d8e72776bdfa984fa0c6f506f279d1136
Michael Sanders <michael.sanders@heliosinteractive.com>
2019-06-19 17:20:00 -0700

    Added character controller and walls to map builder.

---
+++ Assets/_Project/Scripts/Characters/PlayableCharacter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PixelCurio.OccultClassic
+{
+    public class PlayableCharacter : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D _rigidbody;
+        [SerializeField] private Animator _animator;
+        [SerializeField] private Vector2 _movementSpeed;
+
+        private void FixedUpdate()
+        {
+            Vector2 offset = Vector2.zero;
+
+            if (Input.GetKey(KeyCode.A)) offset.x += -_movementSpeed.x;
+            if (Input.GetKey(KeyCode.D)) offset.x += _movementSpeed.x;
+            if (Input.GetKey(KeyCode.W)) offset.y += _movementSpeed.y;
+            if (Input.GetKey(KeyCode.S)) offset.y += -_movementSpeed.y;
+
+            _rigidbody.MovePosition(_rigidbody.position + offset * Time.fixedDeltaTime);
+
+            _animator.SetFloat("HorizontalSpeed", offset.x);
+            _animator.SetFloat("VerticalSpeed", offset.y);
+        }
+    }
+}
--- Assets/_Project/Scripts/Managers/MapBuilder.cs
+++ Assets/_Project/Scripts/Managers/MapBuilder.cs
@@ -7,6 +7,7 @@ namespace PixelCurio.OccultClassic
     public class MapBuilder : IInitializable
     {
         [Inject(Id = "BaseLayer")] private readonly Tilemap _baseLayer;
+        [Inject(Id = "WallLayer")] private readonly Tilemap _wallLayer;
         [Inject(Id = "DefaultPalette")] private readonly MapPalette _defaultPalette;
 
         public void Initialize()
@@ -16,6 +17,10 @@ namespace PixelCurio.OccultClassic
             for (int y = 0; y < _defaultPalette.Dimensions.y; y++)
                 for (int x = 0; x < _defaultPalette.Dimensions.x; x++)
                 {
+                    if (x == 0 || x >= _defaultPalette.Dimensions.x - 1 ||
+                        y == 0 || y >= _defaultPalette.Dimensions.y - 1)
+                        _wallLayer.SetTile(new Vector3Int(x, y, 0), _defaultPalette.WallTiles[0]);
+
                     _baseLayer.SetTile(new Vector3Int(x, y, 0), _defaultPalette.FloorTiles[0]);
                 }
         }
